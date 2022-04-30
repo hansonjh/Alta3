aa877c2f67c86ec2065ea46d22c1ccdd632fa2cc
J Hanson <hansonjh@users.noreply.github.com>
2022-04-30 21:38:27 +0000

    Final rpg prep

--- projects/RPG_final.py
+++ projects/RPG_final.py
@@ -6,27 +6,18 @@
 
 '''
 ********** TO DO **********
-> Add help option to show instructions (222 - 223)
-> Add description of each room (line 216 - 220)
-> health system (199 - 202)
-> attack system
-> PROBLEM using bean!!!!
-> make ball appear after beating Buu....
 > make getting DB a bit trickier
-> make it so the 5 starball falls out of buu when deadi
-> add 'crushing wall' using a timer? or add timer to a game..
-> desc added... maybe add links to DBZ wiki?
+> add 'crushing room' using a timer, they have to answer a riddle to exit...MAYBE
+> add riddle game to Satan city with Hercule to win the ball
+> add desc that when you go to forest you train with someone and get the sword
 '''
-import random
-import dice
-import sys
-import os
-from dbz_questions import dbz_questions
 
-def rolldice(min,max):
-    print("Rolling dice...")
-    global rollnum
-    rollnum= random.randint(min,max)
+# ======================================== IMPORTS ==============================================================================
+from random import randint
+import dice
+import sys # Used for system.exit()
+import os # Used for os.system("clear")
+from dbz_questions import dbz_questions
 
 # get the width ("columns") of the screen
 size = os.get_terminal_size()
@@ -35,9 +26,14 @@ sizelist= list(size)
 # element 0 is the width in columns; multiply times whatever
 # character (=,*,&,etc.) to fill the width of the screen!
 
+def rolldice(min,max):
+    print("Rolling dice...")
+    global rollnum
+    rollnum= random.randint(min,max)
+
 def showInstructions():
-  #print a main menu and the commands
-  print('''
+    #print a main menu and the commands
+    print('''
 ______                            ______       _ _  ______
 |  _  \                           | ___ \     | | ||___  /
 | | | |_ __ __ _  __ _  ___  _ __ | |_/ / __ _| | |   / / 
@@ -46,21 +42,25 @@ ______                            ______       _ _  ______
 |___/ |_|  \__,_|\__, |\___/|_| |_\____/ \__,_|_|_|\_____/
                   __/ |                                   
                  |___/                                    
-================================
-|Collect the Dragon Balls &    |
-|defeat Majin Buu before you   |
-|make your wish at the lookout!|
-================================
-|Commands:                     |
-|  go [ex:north]               |
-|  get [item]                  |
-|  use [item]                  |
-|  teleport 'tele' [location]  |
-|  look                        |
-|  help                        |
-|  quit                        |
-================================
-  ''')
+''')
+    print("=" * sizelist[0])
+    print('''
+OBJECTIVE:
+    Collect the Dragon Balls &    
+    defeat Majin Buu before you   
+    make your wish at the lookout!
+''')
+    print("=" * sizelist[0])
+    print('''
+Commands:                     
+  go [ex:north]               
+  get [item]                  
+  use [item]                  
+  teleport                    
+  look "find ways to go"                      
+  help "show instructions"                       
+  quit                        
+''')
 
 def showStatus():
     #print the player's current status
@@ -88,9 +88,95 @@ def showStatus():
 #starting inventory
 inventory = ['Dragon radar']
 
-#staring health
-health = 100
+#buu stats
+villain = [{'name' : 'buu', 'health' : 50, 'damage' : '1d12'}]
 
+#armory
+armory = {'sword' : 
+            {'damage' : '2d15'},
+          'fist' :
+            {'damage' : '1d6'}
+            }
+
+#staring health
+health = 50
+
+def combat():
+    #monster_ID= randint(0,2)
+
+    global health, inventory, armory, villian
+    round = 1
+    buu_health = villain[0]['health'] 
+
+    print(f"Majin Buu appears in the ring! COMBAT STARTS NOW! (in two episodes...)\n")
+    while True:
+        #print(f"ROUND {round}")
+        print("Player Health: [" + str(health) + "]")
+        print("Majin Buu's Health: [" + str(buu_health) + "]")
+
+        print("Type: RUN, or USE [weapon]")
+        move = input().lower().split() # converts move into a lower-case list to deal with each item in list separately
+        buu_damage = sum(dice.roll(villain[0]['damage']))
+        print("\n=========================")
+
+        if move[0] == 'use':
+            if move[1] == 'fist':
+                player_damage = sum(dice.roll(armory[move[1]]['damage']))
+                print(f"You punched Majin Buu for {player_damage} damage!")
+            if move[1] in inventory: # checks if weapon is in your inventory
+                player_damage = sum(dice.roll(armory[move[1]]['damage']))
+                print(f"You sliced Majin Buu for {player_damage} damage!")
+                if move[1] not in inventory:
+                    print(f"There is no {move[1]} in your inventory!")
+
+        if move[0] == 'run':
+            escape_chance = randint(1,10)
+
+            if escape_chance >= 8:
+                print("You make a flawless escape!")
+                break
+            if escape_chance >= 5:
+                print("You expose your back as you turn and fly away and Buu takes advantage.")
+                print(f"Buu hits you for {buu_damage} damage!")
+                health -= int(buu_damage)
+                if health >= 1:
+                    print("You managed to escape.")
+                #add a place to run to that is not the Tourney grounds    
+                    break
+                if health < 1:
+                    print("You have been destroyed.")
+                    print("\nGAME OVER")
+                    sys.exit()
+            if escape_chance >= 0:
+                print("Majin Buu out-maneuvers you and blast's you! You do not escape.")
+
+        try:
+            buu_health -= int(player_damage)
+        except:
+            pass
+        if buu_health <= 0:
+            print(f"Majin buu lies dead. You are victorious!\n")
+            print('It looks like Buu had a Dragon Ball hiding inside him but it rolled out..')
+            rooms['Tourney grounds']['ball'] = '5 starball'
+            rooms['Kame house']['bean'] = 'senzu bean'
+            del rooms['Tourney grounds']['buu']
+            break
+
+        print(f"Buu hits you for {buu_damage} damage!")
+        print ("=========================\n")
+        round += 1
+        health -= int(buu_damage)
+
+        if health <= 0:
+            print("You have been vanquished! You are dead.")
+            sys.exit()
+
+def encounter():
+    if rooms[currentRoom]['buu']:
+        combat()
+
+
+# ======================================== DICTIONARIES / LISTS ================================================================
 #a dictionary linking a room to other rooms
 #also shows available items and objects
 rooms = {
@@ -100,54 +186,63 @@ rooms = {
                   'south' : 'South city',
                   'east'  : 'East city',
                   'west'  : 'West city',
-                  'desc'  : "Kame House is a house on a very small island in the middle of the sea.\nIt is the home of Master Roshi, and there he is looking at a naughty magazine in his beach chair...\nGo [north, west, south, east] from here.",
+                  'dir'   : 'From here you can go north, east, south and west.',
+                  'desc'  : "Kame House is a house on a very small island in the middle of the sea.\nIt is the home of Master Roshi, and there he is looking at a naughty magazine in his beach chair...",
                 },
-            'North city' : {
+            'North city': {
                   'north' : 'The lookout',
                   'south' : 'Kame house',
                   'ball'  : '1 starball',
-                  'desc' : "North City, also known as Metro North, is one of the large metropolis capitals of Earth.\nIt is surrounded by mountains and a forest.\nDr. Gero\'s Laboratory is in the mountains south of the city.\nGo [north, south] from here.",
+                  'dir'   : 'From here you can go north and south.',
+                  'desc'  : "North City, also known as Metro North, is one of the large metropolis capitals of Earth.\nIt is surrounded by mountains and a forest.\nDr. Gero\'s Laboratory is in the mountains south of the city.",
                 },
-            'The lookout' : {
-                  'south': 'North city',
-                  'ball' : '3 starball',
-                  'desc' : "The Lookout is an ancient platform that is in geostationary orbit in the skies of Earth,\nand directly above Korin Tower",
+            'The lookout': {
+                  'south' : 'North city',
+                  'ball'  : '3 starball',
+                  'dir'   : 'From here you can go south.',
+                  'desc'  : "The Lookout is an ancient platform that is in geostationary orbit in the skies of Earth,\nand directly above Korin Tower",
                },
             'East city' : {
                   'north' : 'Tourney grounds',
-                  'west' : 'Kame house',
+                  'west'  : 'Kame house',
                   'ball'  : '7 starball',
-                  'desc' : 'Welcome to East City, from here you can head north to the Tournanemt Grounds or west to Kame\'s House.', 
+                  'dir'   : 'From here you can go north and west.',
+                  'desc'  : 'Welcome to East City, there\'s not much here...', 
                },
             'Tourney grounds' : {
                   'south' : 'East city',
-                  'ball' : '5 starball',
-                  'buu'  : 'Kid buu',
-                  'desc' : "The World Martial Arts Tournament originated in a festival held since long ago at the temple which now serves as the tournament grounds",
+                  'buu'   : 'Kid buu',
+                  'dir'   : 'From here you can go south.',
+                  'desc'  : "The World Martial Arts Tournament originated in a festival held since long ago at the temple which now serves as the tournament grounds",
                },
             'South city' : {
                   'south' : 'Satan city',
                   'north' : 'Kame house',
                   'ball4' : '4 starball',
-                  'desc' : 'Welcome to South city a great place to pass through. So keep heading south to move on...',
+                  'dir'   : 'From here you can go north and south.',
+                  'desc'  : 'Welcome to South city a great place to pass through. So keep heading south to move on...',
                },
             'Satan city' : {
                   'north' : 'South city',
-                  'ball' : '6 starball',
-                  'desc' : 'Welcome to Satan city, if you look around you might see Hercule acting the fool.\nUnfortunatly, the only place to go is back the way you came.',
+                  'ball'  : '6 starball',
+                  'dir'   : 'From here you can go north.',
+                  'desc'  : 'Welcome to Satan city, if you look around you might see Hercule acting the fool.\nUnfortunatly, the only place to go is back the way you came.',
                },
             'West city' : {
-                  'east' : 'Kame house',
+                  'east'  : 'Kame house',
                   'south' : 'Forest',
                   'ball2' : '2 starball',
-                  'desc' : 'West city is mostly known as the hometown of Bulma and the headquarters of Capsule Corporation.\nDue to its apparent wealth, the city quickly recovers from most incidents, such as being a target of Majin Buu.',
+                  'dir'   : 'From here you can go east and south.',
+                  'desc'  : 'West city is mostly known as the hometown of Bulma and the headquarters of Capsule Corporation.\nDue to its apparent wealth, the city quickly recovers from most incidents, such as being a target of Majin Buu.',
                },
             'Forest' : {
                   'north' : 'West city',
                   'zsword': 'zsword',
+                  'dir'   : 'From here you can go north.',
                   'desc'  : 'You stumble into a random forest and see something shimmering as it extends from the top of a rock...'
             }
          }
+tele_rooms = ['Kame house', 'North city', 'West city', 'South city', 'East city', 'Satan city', 'Forest', 'Tourney grounds', 'The lookout',]
 
 #start at Kame's house
 currentRoom = 'Kame house'
@@ -160,7 +255,8 @@ showInstructions()
 ball_count = 0
 grade = 0
 
-#loop forever until you win
+#========================================= Game Loop ============================================
+
 while True:
 
     showStatus()
@@ -183,21 +279,27 @@ while True:
             #set the current room to the new room
             currentRoom = rooms[currentRoom][move[1]]
         #trying to make buu attack
-            if 'buu' in currentRoom:
-                health -= 10
-                print('Buu hit you so hard you are nearly dead, hurry and fight back')
-                print(health)
+            print(currentRoom)
+            if 'buu' in rooms[currentRoom]:
+                encounter()    
             #there is no way to the new rooms then...
-        else:
+        else:# move[1] not in rooms[currentRoom]:
             print('You can\'t go that way!')
 
     #if they type teleport or tele first
-    if move[0] == 'tele' or 'teleport':
-        #to teleport they need to have the z sword in inventory
-        if move[1] in rooms and 'zsword' in inventory:
-            currentRoom = rooms[move[1]]
+    if move[0] == 'teleport' and 'sword' in inventory:
+        print('Available rooms:')
+        for room_names in tele_rooms:
+            print('>', room_names)
+        room_teleport = input('\nWhere do you want to teleport? ')
+        if room_teleport in rooms.keys():
+            currentRoom = room_teleport
+        else:
+            print('You need the Zsword to be able to teleport')
+
     #if they type 'get' first
     if move[0] == 'get':
+        
         #if the room contains an item, and the item is the one they want to get
         if "ball" in rooms[currentRoom] and move[1] in rooms[currentRoom]['ball']:
             #add the item to their inventory
@@ -208,11 +310,13 @@ while True:
             print('you currently have', ball_count,'Dragon Balls')
             #delete the item from the room
             del rooms[currentRoom]['ball']
+        
         elif 'ball2' in rooms[currentRoom] and move[1] in rooms[currentRoom]["ball2"]:
             print('''You see a showman offering a Dragon Ball for anyone who can score 80% or above on a Dragon Ball quiz!''')
             #do they want to play?
             gamestart = input('Will you accept the challenge? (y/n): ')
             # make the case lower for ease
+            
             if gamestart.lower() == 'n':
                 continue
             else:
@@ -238,6 +342,7 @@ while True:
                         print()
                     else:
                         print("That is not correct. Keep trying!")
+    
                 if grade >= 4: #how they win the game need 4/5 questions right
                     inventory += [move[1]] #add ball to inventory
                     ball_count += 1 #increase ball count for end game win
@@ -285,7 +390,7 @@ while True:
             #delete the bean from dict
             del rooms[currentRoom]['bean']
             #display helpful message to mayb use it...
-            print(move[1]+"\'s", 'are now in your hand, might be good to use one now')
+            print('A Senzu bean is now in your hand, it might be good to use one now')
         #otherwise, if the item isn't there to get
         else:
             #tell them they can't get it
@@ -293,46 +398,48 @@ while True:
  
     if move[0] == 'use':
         #check that they are allowed use this item
-        if move[1] == 'sword' and 'zsword' in inventory and rooms[currentRoom]['buu']:
+        '''if move[1] == 'sword' and 'sword' in inventory and rooms[currentRoom]['buu']:
             print('The Z Sword was very affective! This is much easier than the series looks.')
             rooms['Kame house']['bean'] = 'senzu bean'
             #delete the object from the dict
             del rooms[currentRoom]['buu']
             #if you do not have the sword print this message
         if 'sword' not in inventory:
-            print('You don\'t have a weapon strong enough to defeat Buu...')
+            print('You don\'t have a weapon istrong enough to defeat Buu...')
+        '''
+
         if move[1] == 'bean':
             health = 100
             #remove bean from inventory
             inventory.remove('bean')
             print('You have recovered and your health is back to', health, 'Let\'s get to the lookout quickly...')
-        #else:
-            #print('You don\'t have a weapon strong enough to defeat Buu...')
-   #look broken and references line 187 
+    
     if move[0] == 'look':
-        if 'desc' in rooms[currentRoom]:
+        if 'dir' in rooms[currentRoom]:
             #print the room description
-            print(rooms[currentRoom]['desc'])
+            print("=" * sizelist[0])
+            print(rooms[currentRoom]['dir'])
         else:
             print('You don\'t see anything.')
-    #help also broken and error looks at line 187?...
+    
     if move[0] == 'help':
         showInstructions()
 
-    if move[0] in ['q', 'quit]']:
+    if move[0] == 'q' or move[0] == 'quit':
         print("Are you sure you want to quit? Yes/No")
         quit_query = input('>')
-        if quit_query.lower() in ['y', 'yes']:
+        if quit_query.lower() == 'y' or quit_query.lower() == 'yes':
             print("Thanks for playing!")
             sys.exit()
-        else:
-            pass
+        if quit_query.lower() == 'n' or quit_query.lower() == 'no':
+            continue
 
      ## Define how a player can win
     if currentRoom == 'The lookout' and ball_count == 7 and 'buu' not in rooms['Tourney grounds']:
         wish = input('***You summon Shenron***\nShenron roars! TELL ME YOUR WISH... ') #type your wish here
         print("That is out of my power, but I can can give these project makers passing score!")
         break
+    
     if currentRoom == 'The lookout' and ball_count == 7 and 'buu' in rooms['Tourney grounds']:
         print("You must defeat Majin Buu before making your wish!")
   
